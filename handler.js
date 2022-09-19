353d06d5f6aefaf64935c93310770485c2f51e26
심용현 <sim-yonghyeon@sim-yonghyeon-ui-MacBookPro.local>
2022-09-19 14:11:26 +0900

    init

---
+++ handler.js
@@ -0,0 +1,42 @@
+"use strict";
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.hello2 = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "hello world2",
+            },
+            null,
+            2
+        ),
+    };
+};
+
+module.exports.world = async (event) => {
+    return {
+        statusCode: 201,
+        body: JSON.stringify(
+            {
+                message: "hello world",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
+};
